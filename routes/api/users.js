852522787c6ae8f2f2dfb7894384773725eecf37
Raz <razefron01@gmail.com>
2020-02-24 13:46:15 -0500

    upadte user backend ready to merge

--- routes/api/users.js
+++ routes/api/users.js
@@ -28,7 +28,11 @@ router.post('/register', (req, res) => {
       const newUser = new User({
         handle: req.body.handle,
         email: req.body.email,
-        password: req.body.password
+        password: req.body.password,
+        homeAddress: req.body.homeAddress,
+        workAddress: req.body.workAddress,
+        arriveToWorkBy: req.body.arriveToWorkBy,
+        departWorkBy: req.body.departWorkBy
       })
 
       bcrypt.genSalt(10, (err, salt) => {
@@ -99,11 +103,89 @@ router.get('/current', passport.authenticate('jwt', { session: false }), (req, r
   res.json({
     id: req.user.id,
     handle: req.user.handle,
-    email: req.user.email
+    email: req.user.email,
+    homeAddress: req.user.homeAddress,
+    workAddress: req.user.workAddress,
+    arriveToWorkBy: req.user.arriveToWorkBy,
+    departWorkBy: req.user.departWorkBy
   });
 })
 
+// router.put(
+//   "/:id",
+//   // passport.authenticate("jwt", { session: false }),
+//   (req, res) => {
+//     User.find(req.params.id),
+//       {
+//         handle: req.body.handle,
+//         email: req.body.email,
+//         password: req.body.password,
+//         homeAddress: req.body.homeAddress,
+//         workAddress: req.body.workAddress,
+//         arriveToWorkBy: req.body.arriveToWorkBy,
+//         departWorkBy: req.body.departWorkBy
+//       },
+//       function(err) {
+//         if (err) {
+//           return res.send(err);
+//         }
+//         console.log({ message: "movie updated" });
+//       }
+//     );
+//   }
+// );
 
+router.route("/:id").post(function(req, res) {
+  User.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+        handle: req.body.handle,
+        email: req.body.email,
+        password: req.body.password,
+        homeAddress: req.body.homeAddress,
+        workAddress: req.body.workAddress,
+        arriveToWorkBy: req.body.arriveToWorkBy,
+        departWorkBy: req.body.departWorkBy
+      },
+    function(err, result) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
 
+// router.put("/movies/:id", function(req, res) {
+//   Movie.findOneAndUpdate(
+//     req.params.id,
+//     {
+//       title: "the gift4",
+//       releaseYear: "2012",
+//       director: "stefan",
+//       genre: "horror"
+//     },
+//     function(err) {
+//       if (err) {
+//         return res.send(err);
+//       }
+//       console.log({ message: "movie updated" });
+//     }
+//   );
+// });
+// router.patch("/:id", function(req, res, next) {
+//   Comment.update(
+//     { comment: req.body.comment },
+//     { where: { id: req.params.id } }
+//   )
+//     .then(result => {
+//       res.json(result);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//       next(err);
+//     });
+// });
 
 module.exports = router;
