c1234993f4b807004e5e94ab057604b81bb1775b
Albert Lee <aclee94@gmail.com>
2020-02-25 11:53:37 -0500

    Restructured css

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
-import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
+import MainPageContainer from "./main/main_page_container";
 import "./main.scss";
 
 const App = () => (
   <section className="app-container">
     <Switch>
-      <AuthRoute exact path="/" component={MainPage} />
+      <AuthRoute exact path="/" component={MainPageContainer} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
     </Switch>
