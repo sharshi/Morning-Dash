3d4dcea1b7e0d988c06e6532a25b0c917647b818
lifesscholar <n_granite@hotmail.com>
2020-02-25 18:39:57 -0500

    created Backend Routes for Dark Sky API

--- frontend/src/util/weather_util.js
+++ frontend/src/util/weather_util.js
@@ -1,7 +1,6 @@
-import keys from '../../../config/keys';
 import axios from "axios";
 
 export const weather = (coords) => {
-    return axios.get(`https://api.darksky.net/forecast/${keys.darkSkyAPI}/${coords.lat},${coords.lng}`);
+`https://api.darksky.net/forecast/${keys.darkSkyAPI}/${coords.lat},${coords.lng}`    return axios.get();
 }
 
