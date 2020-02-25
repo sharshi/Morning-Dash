14622d23eb3d909e1501ce6973aa15c3462f59e5
Albert Lee <aclee94@gmail.com>
2020-02-25 11:26:38 -0500

    Restructured css files

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
-import NavBarContainer from "./nav/navbar_container";
-
 import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
+import "./main.scss";
 
 const App = () => (
   <section className="app-container">
