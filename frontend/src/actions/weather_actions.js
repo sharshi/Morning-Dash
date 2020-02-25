2ef9e411ad585bd1cdd065015363a9f0bd7da005
lifesscholar <n_granite@hotmail.com>
2020-02-25 11:17:05 -0500

    created weather util, action, and reducer

--- frontend/src/actions/weather_actions.js
+++ frontend/src/actions/weather_actions.js
@@ -1,3 +1,25 @@
-import keys from '../../../config/keys';
+import * as APIUtil from "../util/weather_util";
 
-keys.darkSkiAPI
+export const RECEIVE_WEATHER = "RECEIVE_WEATHER";
+export const RECEIVE_WEATHER_ERRORS = "RECEIVE_WEATHER_ERRORS";
+
+export const receiveWeather = weather => {
+    return {
+        type: RECEIVE_WEATHER,
+        weather
+    };
+};
+
+export const receiveWeatherErrors = errors => {
+    return {
+        type: RECEIVE_WEATHER_ERRORS,
+        errors
+    }
+}
+
+export const fetchWeather = coords => dispatch => {
+    return APIUtil.weather(coords).then(
+        weather => dispatch(receiveWeather(weather)),
+        errors => dispatch(receiveWeatherErrors(errors))
+    )
+};
--- frontend/src/util/weather_util.js
+++ frontend/src/util/weather_util.js
@@ -1,3 +1,7 @@
 import keys from '../../../config/keys';
+import axios from "axios";
 
-keys.darkSkiAPI
+export const weather = (coords) => {
+    return axios.get(`https://api.darksky.net/forecast/${keys.darkSkyAPI}/${coords.lat},${coords.lng}`);
+}
+
