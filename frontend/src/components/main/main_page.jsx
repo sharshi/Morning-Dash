74c3f4b9e0ecafc786f1e1f2e19c625eece15fd4
Albert Lee <aclee94@gmail.com>
2020-02-26 12:41:00 -0500

    Map user state into main page

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -118,7 +118,7 @@ class MainPage extends React.Component {
           </div>
           <div className="commute-summary">
             <div className="commute-summary-item">
-              Leave for work at 8:22 AM
+              Leave at 8:22 AM for work.
             </div>
             {/* <div className="commute-summary-item">Go home at 7:00 PM</div> */}
             <div className="commute-summary-item">
