48bd94e0a1a2fa0d0b532cff92997f6b18364270
sharshi <sharshib@gmail.com>
2020-02-26 10:06:46 -0500

    keys

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-// const keys = require('../../config/keys');
 const passport = require('passport');
 
 const router = express.Router();
