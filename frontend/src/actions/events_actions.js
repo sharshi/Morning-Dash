ad47c6790cd017d24036ded7cf35de640277cb6c
Raz <razefron01@gmail.com>
2020-02-26 12:10:17 -0500

    switch to master

--- frontend/src/actions/events_actions.js
+++ frontend/src/actions/events_actions.js
@@ -1,1 +1,25 @@
 import * as APIUtil from '../util/google_api_util';
+
+export const RECEIVE_EVENTS = "RECEIVE_EVENTS";
+export const RECEIVE_EVENTS_ERRORS = "RECEIVE_EVENTS_ERRORS";
+
+const receive_events = (events) => ({
+    type: RECEIVE_EVENTS,
+    events
+})
+
+
+
+const receiveEventsErrors = errors => {
+  return {
+    type: RECEIVE_EVENTS_ERRORS,
+    errors
+  };
+};
+
+export const fetchEvents = () => dispatch => {
+  return APIUtil.listUpcomingEvents().then(
+    events => dispatch(receiveWeather(weather)),
+    errors => dispatch(receiveWeatherErrors(errors))
+  );
+};
