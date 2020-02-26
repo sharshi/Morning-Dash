81b683602c0a68b835356f471e0d7f29fc7ab7c2
lifesscholar <n_granite@hotmail.com>
2020-02-26 11:54:23 -0500

    jwtStrategy error

--- config/passport.js
+++ config/passport.js
@@ -2,12 +2,11 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-// const keys = require('../config/keys');
 
 const options = {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = process.env.secretOrKey;
-
+debugger
 module.exports = passport => {
   passport.use(new JwtStrategy(options, (jwt_payload, done) => {
     User.findById(jwt_payload.id)
