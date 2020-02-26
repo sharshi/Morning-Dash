90f77be9fed867bf0ff63663a931af0d3cf3e9aa
Albert Lee <aclee94@gmail.com>
2020-02-26 10:43:54 -0500

    Fix weather reducer bug

--- frontend/src/reducers/weather_reducer.js
+++ frontend/src/reducers/weather_reducer.js
@@ -1,15 +1,18 @@
-import { RECEIVE_WEATHER, RECEIVE_WEATHER_ERRORS } from "../actions/weather_actions";
+import {
+  RECEIVE_WEATHER,
+  RECEIVE_WEATHER_ERRORS
+} from "../actions/weather_actions";
 
 weatherReducer = (state = {}, action) => {
   Object.freeze(state);
-    switch (action.type) {
-      case RECEIVE_WEATHER:
-        return { weather: action.weather };
-      case RECEIVE_WEATHER_ERRORS:
-        return { errors: action.errors };
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case RECEIVE_WEATHER:
+      return { weather: action.weather };
+    case RECEIVE_WEATHER_ERRORS:
+      return { errors: action.errors };
+    default:
+      return state;
+  }
 };
 
-export const weatherReducer;
+export default weatherReducer;
