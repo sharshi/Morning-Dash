7a9dbcf979872258d7934c00440ccff1df296ccc
Raz <razefron01@gmail.com>
2020-02-27 18:19:08 -0500

    ignore

--- frontend/src/components/calendar/calendar.jsx
+++ frontend/src/components/calendar/calendar.jsx
@@ -100,7 +100,7 @@ class Calendar extends React.Component {
   createEvent() {
     if (this.props.events.length > 0) {
       return this.props.events.map(event => (
-        <a key={event.htmlLink} target='_blank' href={event.htmlLink}>{`${event.start.dateTime ? event.start.dateTime.slice(11, 16) : `All Day`} - ${event.summary}`}</a>
+        <Link to={event.htmlLink}>{`${event.start.dateTime.slice(11, 16) || `All Day`} - ${event.summary}`}</Link>
       ));
     } else {
       return "";
